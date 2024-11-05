b111ccb404ee3b159da17c7b7b1e6d0d2fc15ae6
Bhawneet1 <bsingh_be22@thapar.edu>
2024-11-05 13:08:54 +0530

    Update

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,11 +1,20 @@
 import Chai from "./Chai"
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
 function App() {
+  const username ="chai aur code"
   return (
     //<chai/>//ik hi element return kar sakte hai
     <>
       <Chai/>
-      <h1>Chai aur React</h1>
+      <h1>Chai aur React | {username}</h1>
+      {/* variable inject */}
       <p>Check para</p>
     </>//div kai bina bhi
   )
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,9 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App</h1>
+//     </div>
+//   )
+// }
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click to go to google"
+// }
+//function execute hota hai par yeh object hai
+const another_element = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+//from react not custom
+const anotherUser ="Chai aur react"
+const reactElement = React.createElement(
+  'a',//tag
+  {
+    href: "https://google.com",
+    target: "_blank"
+  },//props
+  'Click me to visit google',//children
+  anotherUser//variable
+)
+//no if else or loops
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <reactElement />matlab iss ch hi convert honda hai assi direct eh hi de dita */
+      // <MyApp/> esse direct function likh sakte hai
+      // <reactElement/>
+      //function execute hota hai par yeh object hai
+      //ReactElement//donot expect object therefore not working error
+      //another_element
+      // reactElement
+      <App/>
+    }
   </StrictMode>,
 )
